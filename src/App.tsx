fd5f367d7dc518f6fbb5043502ac342919814d53
Yared Kassahun <ykassahun07@gmail.com>
2025-02-11 21:24:58 +0300

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,27 @@ import {
   Grid,
   GridItem,
   Show,
+  Theme,
 } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateRows="2" templateColumns="4" gap={"0"}>
+    <Grid
+      templateAreas={{
+        base: `'`,
+      }}
+    >
       <GridItem paddingLeft={"10px"} colSpan={6} rowSpan={1} h={"100%"}>
         <NavBar />
       </GridItem>
       <GridItem colSpan={3} rowSpan={1} bg="red" h={"100%"}>
         Main
       </GridItem>
-      <Show when={"lg"}>
-        <GridItem colSpan={3} rowSpan={1} bg="yellow" h={"100%"}>
-          Aside
-        </GridItem>
-      </Show>
+
+      <GridItem colSpan={3} rowSpan={1} bg="yellow" h={"100%"}>
+        Aside
+      </GridItem>
     </Grid>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import { Provider } from "@/components/ui/provider";
 import "./index.css";
 import App from "./App.tsx";
 
+import { Theme } from "@chakra-ui/react";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <Theme appearance="dark">
+        <App />
+      </Theme>
     </Provider>
   </StrictMode>
 );
