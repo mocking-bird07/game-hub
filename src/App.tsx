8bf86925178ed2b2f8b55764725d1046867dd4f3
Yared Kassahun <ykassahun07@gmail.com>
2025-02-11 20:21:14 +0300

    build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,19 @@ import {
   GridItem,
   Show,
 } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Grid templateRows="2" templateColumns="4" gap={"0"}>
-      <GridItem colSpan={6} rowSpan={1} bg="blue" h={"40px"}>
-        Nav
+      <GridItem paddingLeft={"10px"} colSpan={6} rowSpan={1} h={"100%"}>
+        <NavBar />
       </GridItem>
-      <GridItem colSpan={3} rowSpan={1} bg="red" h={"100px"}>
+      <GridItem colSpan={3} rowSpan={1} bg="red" h={"100%"}>
         Main
       </GridItem>
       <Show when={"lg"}>
-        <GridItem colSpan={3} rowSpan={1} bg="yellow" h={"100px"}>
+        <GridItem colSpan={3} rowSpan={1} bg="yellow" h={"100%"}>
           Aside
         </GridItem>
       </Show>
